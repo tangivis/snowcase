cd9ad1017e9dc0927f837e38f16aba4be2c08aa3
ivis_arch <tang.zhengliang@ivis-sh.com>
2022-06-13 21:20:08 +0800

    features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import assets from '../assets';
+import styles from '../styles/Global';
 
 const Features = () => {
   return (
-    <div>
-      Features
+    <div className={`${styles.section} ${styles.bgPrimary} banner03`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div>
+          <h1 className={`${styles.h1Text} ${styles.whiteText}`}>
+            Technologies
+          </h1>
+          <p className={`${styles.pText} ${styles.whiteText}`}>
+            ProNet has been deployed using a cross-platform techology, React Native.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -3,20 +3,32 @@ import styles from "../styles/Global";
 import Button from './Button';
 import assets from '../assets';
 
-const SectionWrapper = ({ title, description, showBtn, mockupImg, banner }) => {
+const SectionWrapper = ({ title, description, showBtn, mockupImg, banner, reverse }) => {
   return (
-    <div className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}>
-      <div className={`flex items-center ${styles.boxClass}`}>
-      </div>
-        <div className={`${styles.descDiv}`}>
-          <h1 className={`${styles.h1Text}`}>{title}</h1>
-          <p className={`${styles.descriptionText}`}>{description}</p>
+    <div className={`min-h-screen ${styles.section}
+      ${reverse ? styles.bgWhite : styles.bgPrimary}
+      ${banner}`}>
+    <div className={`flex items-center
+      ${ reverse ? styles.boxReverseClass : styles.boxClass}
+      w-11/12 sm:w-full minmd:w-3/4`}>
+        <div className={`${styles.descDiv}
+          ${reverse ? " fadeRightMini" : " fadeLeftMini"}
+          ${reverse ? styles.textRight : styles.textLeft}
+        `}>
+          <h1 className={`
+            ${styles.h1Text}
+            ${reverse ? styles.blackText : styles.whiteText}
+          `}>{title}</h1>
+          <p className={`${styles.descriptionText} ${reverse ? styles.blackText : styles.whiteText}`}>{description}</p>
           { showBtn && (<Button
             assetsUrl={assets.expo}
             link="deployed nft marketplace rn"
           />)}
         </div>
-      <img src={mockupImg} alt='mockup' />
+        <div className={`flex-1 ${styles.flexCenter} p-8 sm:px-0`}>
+          <img src={mockupImg} alt='mockup' className={`${reverse ? " fadeLeftMini" : " fadeRightMini"}`}/>
+        </div>
+      </div>
     </div>
   )
 }
